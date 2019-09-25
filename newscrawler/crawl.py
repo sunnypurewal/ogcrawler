095e6cc731e95789e7fb9d2829ff4f2e71ab646a
Sunny Purewal <sunnypurewal@gmail.com>
2019-09-25 11:25:15 -0400

    Correctly searching for paragraph elements in an og:article page

--- newscrawler/crawl.py
+++ newscrawler/crawl.py
@@ -1,14 +1,21 @@
-import os
 import sys
-import shutil
 import json
-
-sys.path.append("newscrawler/spiders")
+import logging
+from newscrawler.spiders.articles import ArticleSpider
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
 def main(domainsfile):
+  logging.getLogger("scrapy").propagate = False
   with open(domainsfile, "r") as f:
     domains = json.loads(f.read())
-    print (len(domains["domains"]["politics"]))
+    politics = domains["politics"]
+
+  process = CrawlerProcess(get_project_settings())
+  process.crawl(ArticleSpider, name="articles", domains=politics)
+
+  print ("Starting Crawler Process")
+  process.start()
 
 if __name__ == "__main__":
     main(sys.argv[1])
--- newscrawler/newscrawler/spiders/articles.py
+++ newscrawler/newscrawler/spiders/articles.py
@@ -1,18 +1,35 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import datetime
+import urllib
 import dateparser
-import json
-import sys
-import os
+import datetime
 
 class ArticleSpider(scrapy.Spider):
-  name = 'articles'
+
+  def __init__(self, name=None, *args, **kwargs):
+    super().__init__(name, *args, **kwargs)
+    self.domains = kwargs["domains"]
 
   def start_requests(self):
-    with open("domains.json", "r") as f:
-      domains = json.load(f)
-    print (domains)
+    return [scrapy.Request(url=f"https://www.{d}") for d in self.domains]
 
   def parse(self, response):
-    pass
+    links = response.xpath("//a")
+    print (f"{response.request.url} {len(links)} <a> elements")
+    for link in links:
+      url = link.xpath("@href").get()
+      urlcomponents = urllib.parse.urlparse(url)
+      if len(urlcomponents.scheme) > 0:
+        yield(scrapy.Request(url=url, 
+                             callback=self.parse_article))
+      elif len(urlcomponents.path) > 0:
+        yield(scrapy.Request(url=f"{response.request.url}{urlcomponents.path}", 
+                             callback=self.parse_article))
+
+  def parse_article(self, response):
+    ogtype = response.xpath("//meta[@property='og:type']/@content").get()
+    if ogtype != "article": return
+    paragraphs = response.xpath("//p")
+    for paragraph in paragraphs:
+      siblings = paragraph.xpath("following-sibling::p")
+      print(f"This p has {len(siblings)} siblings")
